511bb0de2dc7fb04f6f366435a4200955e259ee6
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2026-07-09 12:18:06 -0700

    Fix rope renderer not moving with player

--- Assets/Scripts/Player/Rope Dart/RopeRenderer.cs
+++ Assets/Scripts/Player/Rope Dart/RopeRenderer.cs
@@ -5,13 +5,22 @@ public class RopeRenderer : Singleton<RopeRenderer>
 {
     [SerializeField] private LineRenderer lineRenderer;
 
+    private List<BindPointObject> currentBindPoints = new List<BindPointObject>();
+
     void Update()
     {
+        for (int i = 0; i < currentBindPoints.Count; i++)
+        {
+            lineRenderer.SetPosition(i, currentBindPoints[i].Position);
+        }
+
         lineRenderer.SetPosition(lineRenderer.positionCount - 1, RopeDartManager.Instance.GetHeadPosition());
     }
-    
+
     public void Reset()
     {
+        currentBindPoints.Clear();
+
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, RopeDartManager.Instance.GetHeadPosition());
         lineRenderer.enabled = true;
@@ -32,6 +41,8 @@ public class RopeRenderer : Singleton<RopeRenderer>
     {
         if (lineRenderer.positionCount == 0) Reset();
 
+        currentBindPoints.Add(point);
+
         lineRenderer.positionCount += 1;
 
         lineRenderer.SetPosition(lineRenderer.positionCount - 1, RopeDartManager.Instance.GetHeadPosition());
@@ -43,6 +54,8 @@ public class RopeRenderer : Singleton<RopeRenderer>
     {
         if (lineRenderer.positionCount > 0)
         {
+            currentBindPoints.RemoveAt(currentBindPoints.Count - 1);
+
             lineRenderer.positionCount -= 1;
             lineRenderer.SetPosition(lineRenderer.positionCount - 1, RopeDartManager.Instance.GetHeadPosition());
         }
